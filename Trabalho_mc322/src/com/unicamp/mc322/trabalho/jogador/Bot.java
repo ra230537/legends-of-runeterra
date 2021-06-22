6cfda8541fb47eb107713cccc583ef46632aa6a4
Lucas <l182278@dac.unicamp.br>
2021-06-22 19:40:16 -0300

    add nova subclasse bot(usada no jogador vs computador)

---
+++ Trabalho_mc322/src/com/unicamp/mc322/trabalho/jogador/Bot.java
@@ -0,0 +1,7 @@
+package com.unicamp.mc322.trabalho.jogador;
+
+public class Bot extends Jogador {
+	//quando for para jogar vs computador criar classe bot, com metodos para jogar automatico;
+	
+	
+}
