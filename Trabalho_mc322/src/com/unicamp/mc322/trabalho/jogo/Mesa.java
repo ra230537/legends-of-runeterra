e6d32d4ed8f88c23216c58e6ac3501bd4e55d0e8
Mateus-Diniz <84398286+Mateus-Diniz@users.noreply.github.com>
2021-06-29 17:35:57 -0300

    Comecei a mesa
    fiquei meio confuso em como vamos contralor a fluxo das rodadas e dos rounds, se vai ser pela mesa ou pelo jogo

--- Trabalho_mc322/src/com/unicamp/mc322/trabalho/jogo/Mesa.java
+++ Trabalho_mc322/src/com/unicamp/mc322/trabalho/jogo/Mesa.java
@@ -1,5 +1,19 @@
 package com.unicamp.mc322.trabalho.jogo;
 
-public class Mesa {
+import com.unicamp.mc322.trabalho.jogador.Jogador;
+import com.unicamp.mc322.trabalho.jogo.expansao.carta.Carta;
 
+public class Mesa {
+	private Carta[][] CartasNaMesa = new Carta[3][5];
+	private Jogador[] jogadores	= new Jogador[1];
+	
+	public Mesa(Jogador J1, Jogador J2) {
+		
+		jogadores[0] = J1;
+		jogadores[1] = J2;
+	}
+	
+	public void JogarCarta(Carta c, Jogador J) {
+		
+	}
 }
